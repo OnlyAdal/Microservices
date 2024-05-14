947d0fe734f14b05cffffaebbd25e05bf5fa6279
punto CB <puntocb.sf@gmail.com>
2024-05-13 18:32:58 -0600

    Agregar archivos de proyecto.

---
+++ Servicios.api.Libreria/Controllers/LibreriaAutorController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.api.Libreria.Core.Entities;
+using Servicios.api.Libreria.Repository;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Servicios.api.Libreria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibreriaAutorController : ControllerBase
+    {
+        private readonly IMongoRepository<AutorEntity> _autorGenericoRepository;
+
+        public LibreriaAutorController(IMongoRepository<AutorEntity> autorGenericoRepository)
+        {
+                _autorGenericoRepository = autorGenericoRepository;
+        }
+
+        [HttpGet("getAll")]
+        public async Task<ActionResult<IEnumerable<AutorEntity>>> Get()
+        {
+            return Ok(await _autorGenericoRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AutorEntity>>GetById(string Id)
+        {
+            var autor = await _autorGenericoRepository.GetById(Id);
+            return Ok(autor);
+        }
+
+        [HttpPost]
+        public async Task Post(AutorEntity autor)
+        {
+            await _autorGenericoRepository.InsertDocument(autor);
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(string Id, AutorEntity autor)
+        {
+            autor.Id = Id;
+            await _autorGenericoRepository.UpdateDocument(autor);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(string id)
+        {
+            await _autorGenericoRepository.DeleteById(id);
+        }
+
+        [HttpPost("pagination")]
+        public async Task<ActionResult<PaginationEntity<AutorEntity>>> PostPagination(PaginationEntity<AutorEntity> pagination)
+        {
+            var resultados = await _autorGenericoRepository.PaginationByFilter(
+                                             pagination
+                                             );
+            return Ok(resultados);
+               
+        }
+    }
+}
